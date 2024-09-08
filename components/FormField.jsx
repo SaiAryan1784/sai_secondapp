25870c58f3e72f52b8822b68cfbb8431bb675b93
SaiAryan1784 <saiaryan.goswami1784@gmail.com>
2024-09-08 20:56:41 +0530

    firstApp

---
+++ components/FormField.jsx
@@ -0,0 +1,14 @@
+import { View, Text } from 'react-native'
+import React from 'react'
+
+const FormField = ({ title, value, placeholder, handleChangeTest, otherStyles, ...props}) => {
+  return (
+    <View className={`space-y-2 ${otherStyles}`}>
+      <Text className='text-base text-gray-100 font-pmedium'>{title}</Text>
+
+      <View className="w-full h-16 px-4 bg-black-100"></View>
+    </View>
+  )
+}
+
+export default FormField
